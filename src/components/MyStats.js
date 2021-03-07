53b61f88ef6f8de83711c7f744cb8b7868ed1953
Jason Williams <jwilliamstap@gmail.com>
2021-03-07 18:17:55 -0500

    added GameCard component to handle multiple games and some general css layout

--- src/components/MyStats.js
+++ src/components/MyStats.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { loadUsers } from '../reducers';
+import GameCard from './GameCard';
 
 export class MyStats extends Component{
   constructor(){
@@ -7,7 +9,12 @@ export class MyStats extends Component{
     this.state = {};
   }
 
+  componentDidMount(){
+    this.props.loadUsers();
+  }
+
   render(){
+    const { user, game, request } = this.props;
     return (
         <div className='container'>
           <div className='header'>
@@ -21,18 +28,36 @@ export class MyStats extends Component{
               <h3>Loses:</h3>
             </div>
           </div>
+          <div className='card'>
+            <GameCard game={game} request={request}/>
+            <GameCard game={game} request={request}/>
+          </div>
         </div>
     );
   }
 }
 
-const mapStateToProps = ({ users, games, requests }) => {
-  console.log(users)
-  console.log('games', games)
-  console.log('requests', requests)
-  return {};
+const mapStateToProps = ({users, games, requests, match}) => {
+  const user = users.find( user => user.id === 4 ) || {};  
+  const request = requests.find( request => request.id === 24 ) || {};  
+  const game = games.find( game => game.id * 1 === 1 ) || {};  
+  console.log(user)
+  console.log('requests', request)
+  console.log('games', game)
+
+  return {
+    user: user,
+    game: game,
+    request: request
+  };
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    loadUsers: () => dispatch(loadUsers)
+  }
 }
 
 
-export default connect(mapStateToProps)(MyStats);
+export default connect(mapStateToProps, mapDispatch)(MyStats);
 
