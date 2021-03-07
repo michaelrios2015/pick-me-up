a187bd2447705f61f9d797e60d53d9cb64e15ab3
Jason Williams <jwilliamstap@gmail.com>
2021-03-07 18:32:48 -0500

    cleaned up MyStat code for pull request

--- src/components/MyStats.js
+++ src/components/MyStats.js
@@ -1,31 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { loadUsers } from '../reducers';
 import GameCard from './GameCard';
 
 export class MyStats extends Component{
-  constructor(){
-    super();
-    this.state = {};
-  }
-
-  componentDidMount(){
-    this.props.loadUsers();
-  }
 
   render(){
     const { user, game, request } = this.props;
     return (
         <div className='container'>
           <div className='header'>
-            <h1>My Stats</h1>
+            {
+              //replace email with user's name
+            }
+            <h1>{user.email}: Stats</h1>
           </div>
           <div className='sub-header'>
             <div>
+              {
+                // need to calculate total wins and losses
+              }
               <h3>Wins:</h3>
             </div>
             <div>
-              <h3>Loses:</h3>
+              <h3>Losses:</h3>
             </div>
           </div>
           <div className='card'>
@@ -37,13 +34,12 @@ export class MyStats extends Component{
   }
 }
 
-const mapStateToProps = ({users, games, requests, match}) => {
-  const user = users.find( user => user.id === 4 ) || {};  
-  const request = requests.find( request => request.id === 24 ) || {};  
-  const game = games.find( game => game.id * 1 === 1 ) || {};  
-  console.log(user)
-  console.log('requests', request)
-  console.log('games', game)
+const mapState = ({users, games, requests, match}) => {
+  //hardcoded user, request, and game to have data to work with;
+  //need to setup url path for a specific user id
+  const user = users.find( user => user.id === 4 ) || {};
+  const request = requests.find( request => request.id === 24 ) || {};
+  const game = games.find( game => game.id * 1 === 1 ) || {};
 
   return {
     user: user,
@@ -52,12 +48,6 @@ const mapStateToProps = ({users, games, requests, match}) => {
   };
 }
 
-const mapDispatch = (dispatch) => {
-  return {
-    loadUsers: () => dispatch(loadUsers)
-  }
-}
 
-
-export default connect(mapStateToProps, mapDispatch)(MyStats);
+export default connect(mapState)(MyStats);
 
