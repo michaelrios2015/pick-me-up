34dd70730aad3a2d22eda37c730d0670cb20062f
Taylor McKeel <josephmckeel1993@gmail.com>
2021-03-08 21:35:41 -0500

    added api functionality

--- src/components/RequestForm.js
+++ src/components/RequestForm.js
@@ -14,28 +14,55 @@ export class RequestForm extends React.Component {
   constructor(props){
     super(props)
     this.state = { 
-
+      zipcode: '',
+      showCourts: false,
+      courts: []
     }
+    this.handleInputs = this.handleInputs.bind(this)
+    this.courtSubmit = this.courtSubmit.bind(this)
   }
   async componentDidMount(){
-    const courts =  (await axios.get(`https://data.cityofnewyork.us/resource/9wwi-sb8x.json?$$app_token=${API_TOKEN}&basketball=Yes`)).data
-    console.log(courts)
+  
+  }
+
+  handleInputs(ev){
+    const {name, value} = ev.target
+    this.setState({[name] : value})
+  }
+  async courtSubmit(ev){
+    ev.preventDefault()
+    const courts =  (await axios.get(`https://data.cityofnewyork.us/resource/9wwi-sb8x.json?$$app_token=${API_TOKEN}&basketball=Yes&zipcode=${this.state.zipcode}`)).data
+    this.setState({courts: courts, showCourts: true})
   }
   render(){
+    console.log(this.state)
+
     return(
       <div id='requestBox'>
         <h1>Pick Up a Game</h1>
         <h2>Pull name and display here</h2>
         <form>
-          <label for='court'>Court:</label>
-          <select>
-            <option value='court1'>Court 1</option>
-          </select>
-          <label for='date'>Date:</label>
-          <input type="date" id="time" name="time"/>
-          <label for='time'>Time:</label>
-          <input type="time" id="time" name="time" min="06:00" max="20:00" />
-          <button>Pick Up!</button>
+          {!this.state.showCourts ? (
+            <div>
+              <label for='zipcode'>Zipcode:</label>
+              <input type="text" id="zipcode" name="zipcode" onChange={this.handleInputs}/>
+              <button onClick={this.courtSubmit}>Find Courts</button>
+            </div>
+          ) : (
+            <div>
+              <label for='court'>Court:</label>
+              <select>
+                {this.state.courts.map((court, idx)=>{
+                  return(<option value={court.gispropnu}>Court: {idx +1}</option>)
+                })}
+              </select>
+              <label for='date'>Date:</label>
+              <input type="date" id="time" name="time"/>
+              <label for='time'>Time:</label>
+              <input type="time" id="time" name="time" min="06:00" max="20:00" />
+              <button>Pick Up!</button>
+            </div>
+          )}
         </form>
       </div>
     )
