fdeb208f20290803c23d5d759a1027ad4aabfb22
Michael Rios <michael.rios093@gmail.com>
2021-03-14 20:21:18 -0400

    a stoping point

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -10,7 +10,8 @@
 //   }
 
 //   onMarkerClick(props, marker, e) {
-//     console.log(this);
+//     console.log(marker);
+    
 //     // ..
 //   }
 
@@ -36,19 +37,16 @@
 //     onClick={this.onMarkerClick}
 //     title={'The marker`s title will appear as a tooltip.'}
 //     name={'SOMA'}
-//     position={{lat: 37.778519, lng: -122.405640}} />
-//   <Marker
-//     name={'Dolores park'}
-//     position={{lat: 37.759703, lng: -122.428093}} />
-//   <Marker />
-//   <Marker
-//     name={'Your position'}
-//     position={{lat: 37.762391, lng: -122.439192}}
-//     icon={{
-//       url: "/path/to/custom_icon.png",
-//       anchor: new google.maps.Point(32,32),
-//       scaledSize: new google.maps.Size(64,64)
-//     }} />
+//     position={{lat: 37.778519, lng: -122.405640}}>
+//     <InfoWindow
+//     visible={true}
+//     // style={styles.infoWindow}
+//     >
+//       <div >
+//         <p>Click on the map or drag the marker to select location where the incident occurred</p>
+//       </div>
+//   </InfoWindow>
+//   </Marker>
 // </Map>
 //   );
 //   }
