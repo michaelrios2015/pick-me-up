57f5253320a9fb19328f823de7c4b45949ffc23b
Michael Rios <michael.rios093@gmail.com>
2021-03-17 05:56:51 -0400

    found bug

--- server/api/index.js
+++ server/api/index.js
@@ -7,7 +7,7 @@ const path = require("path");
 const axios = require("axios");
 const {
 	db,
-	models: { User, Request, Game, User_Game },
+	models: { User, Request, Game, UserGame },
 } = require("../db");
 // i think there is a way to get it from db...?
 const { Op } = require("sequelize");
@@ -320,11 +320,12 @@ app.delete("/api/games/:id", async (req, res, next) => {
 //gets all user_games
 app.get("/api/user_games", async (req, res, next) => {
 	try {
-		res.send(await User_Game.findAll({ include: [User, Game] }));
+		res.send(await UserGame.findAll({ include: [User, Game] }));
 	} catch (ex) {
 		next(ex);
 	}
 });
+
 
 //gets players of a single game
 app.get('/api/user_games/:gameId/players', async(req, res, next)=> {
