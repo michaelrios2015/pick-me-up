ac4a2f082be06a7558a4222084368b8742ccd91f
Michael Rios <michael.rios093@gmail.com>
2021-03-20 08:58:36 -0400

    mac friendly

--- server/db/index.js
+++ server/db/index.js
@@ -12,10 +12,10 @@ if(process.env.LOGGING === 'true'){
   delete config.logging
 }
 
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:JerryPine@localhost/basketball');
-// const db = new Sequelize(
-// 	process.env.DATABASE_URL || "postgres://localhost/basketball", config
-// );
+// const db = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:JerryPine@localhost/basketball');
+const db = new Sequelize(
+	process.env.DATABASE_URL || "postgres://localhost/basketball", config
+);
 
 // Auth
 const bcrypt = require("bcrypt");
