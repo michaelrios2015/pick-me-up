3a2e8fa24ed0d48b2f9dc28627fe62400ed3f8af
Jason Williams <jwilliamstap@gmail.com>
2021-03-20 11:59:16 -0400

    updated for merge

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,5 @@
 //could be split into models and database
 const Sequelize = require("sequelize");
-<<<<<<< HEAD
 const { INTEGER, STRING, BOOLEAN, ENUM, DATE, BIGINT } = Sequelize;
 
 
@@ -17,13 +16,11 @@ if(process.env.LOGGING === 'true'){
 const db = new Sequelize(
 	process.env.DATABASE_URL || "postgres://localhost/basketball", config
 );
-=======
-const { INTEGER, STRING, BOOLEAN, ENUM, DATE} = Sequelize;
+
 const db = require('./db');
 const Game = require('./models/Game');
 const Request = require('./models/Request');
 const UserGame = require('./models/UserGame');
->>>>>>> master
 
 // Auth
 const bcrypt = require("bcrypt");
@@ -63,45 +60,6 @@ User.beforeCreate(async (user, options) => {
 	user.password = await bcrypt.hash(user.password, 10);
 });
 
-<<<<<<< HEAD
-//Everything has been simplified to get us started :)
-const Request = db.define(
-	"request",
-	{
-		// this will need to be changed starting off really restirctive to  make testing
-		// easier will need to replaced with LAT, LONG I assume
-		location: {
-			type: ENUM("COURT 1", "COURT 2", "COURT 3", "COURT 4", "COURT 5"),
-		},
-		// I think date and time can go together, keeping it very simple at the moment
-		// just time date will be added back again very restrive for testing
-		time: {
-			type: INTEGER,
-		},
-		//it would be better if it just checked the time to see if it was an
-		//open and closed request but this should work for now
-		open: {
-			type: BOOLEAN,
-			defaultValue: true,
-		},
-		team: {
-			type: STRING,
-		},
-		waitlist: {
-			type: BOOLEAN,
-			defaultValue: false,
-		},
-		baskets: {
-			type: INTEGER,
-		}
-	},
-	{ timestamps: false }
-);
-
-
-
-=======
->>>>>>> master
 module.exports = {
   // Include your models in this exports object as well!
   db,
