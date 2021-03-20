6739560380f6ffacbb74621e29f5c8f03ed0b9af
Jason Williams <jwilliamstap@gmail.com>
2021-03-20 11:19:18 -0400

    updated Game model for merge

--- server/db/models/Game.js
+++ server/db/models/Game.js
@@ -1,6 +1,6 @@
 const db = require('../db')
 const Sequelize = require('sequelize')
-const { STRING, FLOAT, INTEGER, ENUM, BOOLEAN, DATE } = Sequelize;
+const { STRING, FLOAT, INTEGER, ENUM, BOOLEAN, DATE, BIGINT } = Sequelize;
 
 const Game = db.define('game', {
     //Once matching does not need to be exact time/date, and location will need 
@@ -23,22 +23,25 @@ const Game = db.define('game', {
     // this will need to be changed starting off really restirctive to  make testing
     // easier will need to replaced with LAT, LONG I assume
     location: {  
-          type: STRING
+      type: STRING
       // type: ENUM('COURT 1', 'COURT 2', 'COURT 3','COURT 4','COURT 5'), 
     },
     // I think date and time can go together, keeping it very simple at the moment
     // just time date will be added back again very restrive for testing 
     time: { 
-        type: STRING
+      type: STRING
+      //type: BIGINT
     },
-  
-      date: {
-          type: DATE
-      },
-  
-      host: {
-          type: STRING
-      }
+    date: {
+      type: DATE
+    },
+    host: {
+      type: STRING
+    },
+    maxPlayerCount: {
+      type: INTEGER,
+      defaultValue: 2
+    }
       
   },{ timestamps: false });  
 
