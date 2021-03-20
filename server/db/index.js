095693126327b71cf04c38d1c1ee43e9ac7a7227
Jason Williams <jwilliamstap@gmail.com>
2021-03-20 11:26:50 -0400

    update index

--- server/db/index.js
+++ server/db/index.js
@@ -98,48 +98,7 @@ const Request = db.define(
 	{ timestamps: false }
 );
 
-const Game = db.define('game', {
-  //Once matching does not need to be exact time/date, and location will need 
-  //to be put back 
-  winner: { 
-    type: STRING, 
-  },  
-  finalScore: { 
-    type: STRING, 
-  },
-  // this should not be necessary, but seems like a nice safety measure for the moment
-  done:{
-    type:BOOLEAN,
-    defaultValue: false
-  },
-  open: {
-    type: BOOLEAN,
-    defaultValue: true
-  },
-  // this will need to be changed starting off really restirctive to  make testing
-  // easier will need to replaced with LAT, LONG I assume
-  location: {  
-    type: ENUM('COURT 1', 'COURT 2', 'COURT 3','COURT 4','COURT 5'), 
-  },
-  // I think date and time can go together, keeping it very simple at the moment
-  // just time date will be added back again very restrive for testing 
-	// Jason: We can possibly use the integer in miliseconds to translate into standard time/date 
-  time: { 
-      type: BIGINT
-  },
-	maxPlayerCount: {
-		type: INTEGER,
-		defaultValue: 2
-	}
-},{ timestamps: false });  
 
-// we are not using this for the moment but going to leave in just in case
-//this is the through table to connect users to a game 
-const UserGame = db.define('usergame', {
-  team: {
-    type: STRING,
-  }
-},{ timestamps: false });
 
 =======
 >>>>>>> master
