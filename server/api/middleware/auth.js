2457961bbbd090ff23ee512f74ce5dfbf0aeb08a
Kokko Tso <kokkotso@gmail.com>
2021-03-24 22:59:07 -0400

    feature (routes): add passport auth middleware

---
+++ server/api/middleware/auth.js
@@ -0,0 +1,40 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+// Import User model
+const {
+	models: { User },
+} = require("../../db");
+
+// Password Hashing
+const bcrypt = require("bcrypt");
+
+passport.use(
+	new LocalStrategy(
+		// options
+		{ usernameField: "email", passwordField: "password" },
+		// verify callback
+		async function (email, password, done) {
+			try {
+				const user = await User.findOne({
+					where: { email: email },
+				});
+
+				if (!user) {
+					return done(null, false, { message: "Incorrect email address" });
+				}
+
+				const auth = await user.validatePassword(password);
+
+				if (!auth) {
+					return done(null, false, { message: "Incorrect password" });
+				}
+
+				return done(null, user);
+			} catch (err) {
+				console.log(err);
+				return done(null, false, { message: err });
+			}
+		}
+	)
+);
