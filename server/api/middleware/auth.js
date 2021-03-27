3f3b7223065bcf38062a6b496dce00cbf5d33371
Kokko Tso <kokkotso@gmail.com>
2021-03-26 23:20:41 -0400

    fix (authentication) : strip password data before returning to client

--- server/api/middleware/auth.js
+++ server/api/middleware/auth.js
@@ -31,7 +31,12 @@ passport.use(
 					return done(null, false, { message: "Incorrect password" });
 				}
 
-				return done(null, user);
+				const sanitizedUser = {
+					id: user.id,
+					email: user.email,
+				};
+
+				return done(null, sanitizedUser);
 			} catch (err) {
 				console.log(err);
 				return done(null, false, { message: err });
