32dd12a26e0b8ac53aa40570e40cc2d35a4c05fa
Jason Williams <jwilliamstap@gmail.com>
2021-03-29 20:14:03 -0400

    merged with master

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const { db, models: { User, Request, Game, UserGame } } = require('./db');
-const app = require('./api')
-const syncAndSeed = require('./db/syncAndSeed')
+const app = require('./api');
+const syncAndSeed = require('./db/syncAndSeed');
+const ws = require('ws');
 
 const init = async () => {
 	try {
@@ -12,7 +13,20 @@ const init = async () => {
       await db.sync()
     }
 		const port = process.env.PORT || 3000;
-		app.listen(port, () => console.log(`listening on port ${port}`));
+		const server = app.listen(port, () => console.log(`listening on port ${port}`));
+		const webSocketServer = new ws.Server({ server });
+
+		let sockets = [];
+
+		webSocketServer.on('connection', (socket)=> {
+			sockets.push(socket);
+			socket.on('message', (data)=> {
+				sockets.filter(s => s !== socket).forEach(s => s.send(data));
+			})
+			socket.on('close', ()=> {
+				sockets = sockets.filter(s => s !== socket);
+			})
+		})
 	} catch (ex) {
 		console.log(ex);
 	}
