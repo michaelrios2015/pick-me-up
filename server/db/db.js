f17ba33670426559b89d6facc1a8adeb3c37ea92
Michael Rios <michael.rios093@gmail.com>
2021-03-29 20:04:18 -0400

    Mac friendly

--- server/db/db.js
+++ server/db/db.js
@@ -31,9 +31,9 @@ if(process.env.LOGGING){
 }
 
 // const db = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:JerryPine@localhost/basketball', config);
-const db = new Sequelize('basketball', 'postgres', 'master', {
-  dialect: 'postgres'
-})
+// const db = new Sequelize('basketball', 'postgres', 'master', {
+//   dialect: 'postgres'
+// })
 const db = new Sequelize(
 	process.env.DATABASE_URL || "postgres://localhost/basketball", config
 );
